35f2c47e33395d926ed76f1ac64b0436540513c3
MJ <eoqkrskfk94@naver.com>
2021-05-26 00:08:51 +0900

    [Add] firebase에서 article 정보 불러오기

--- app/src/main/java/com/mj/aop_part3_chapter06/home/HomeFragment.kt
+++ app/src/main/java/com/mj/aop_part3_chapter06/home/HomeFragment.kt
@@ -5,18 +5,74 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.ChildEventListener
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.mj.aop_part3_chapter06.DBKey.Companion.DB_ARTICLES
 import com.mj.aop_part3_chapter06.R
+import com.mj.aop_part3_chapter06.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
 
+    private val auth: FirebaseAuth by lazy {
+        Firebase.auth
+    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    private val articleList = mutableListOf<ArticleModel>()
+    private val listener = object: ChildEventListener {
+        override fun onChildAdded(snapshot: DataSnapshot, previousChildName: String?) {
+            val articleModel = snapshot.getValue(ArticleModel::class.java)
+            articleModel ?: return
+            articleList.add(articleModel)
+            articleAdapter.submitList(articleList)
+        }
+
+        override fun onChildChanged(snapshot: DataSnapshot, previousChildName: String?) {}
+
+        override fun onChildRemoved(snapshot: DataSnapshot) {}
+
+        override fun onChildMoved(snapshot: DataSnapshot, previousChildName: String?) {}
+
+        override fun onCancelled(error: DatabaseError) {}
+
+    }
+    private lateinit var articleDB: DatabaseReference
+    private var binding: FragmentHomeBinding? = null
+    private lateinit var articleAdapter: ArticleAdapter
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fragmentHomeBinding = FragmentHomeBinding.bind(view)
+        binding = fragmentHomeBinding
+
+        articleList.clear()
+        articleDB = Firebase.database.reference.child(DB_ARTICLES)
+
+        articleAdapter = ArticleAdapter()
+
+        fragmentHomeBinding.articleRecyclerView.layoutManager = LinearLayoutManager(context)
+        fragmentHomeBinding.articleRecyclerView.adapter = articleAdapter
+
+        articleDB.addChildEventListener(listener)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        articleAdapter.notifyDataSetChanged()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        articleDB.removeEventListener(listener)
     }
 
 
